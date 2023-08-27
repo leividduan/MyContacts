2f3afe4372f7a91737ec69c6c490f31be299d9c3
Deivid <deivid.cardoso@weduka.com.br>
2023-08-27 20:52:42 -0300

    use container/presentation

--- src/hooks/useErrors.js
+++ src/hooks/useErrors.js
@@ -1,23 +1,23 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 export default function useErrors() {
   const [errors, setErrors] = useState([]);
 
-  function setError({ field, message }) {
+  const setError = useCallback(({ field, message }) => {
     const errorAlreadyExists = errors.find((error) => error.field === field);
     if (errorAlreadyExists) {
       return;
     }
     setErrors((prevState) => [...prevState, { field, message }]);
-  }
+  }, [errors])
 
-  function removeError(fieldName) {
+  const removeError= useCallback((fieldName) => {
     setErrors((prevState) => prevState.filter((error) => error.field !== fieldName));
-  }
+  }, [])
 
-  function getErrorMessageByFieldName(fieldName) {
+  const getErrorMessageByFieldName = useCallback((fieldName) => {
     return errors.find((error) => error.field === fieldName)?.message;
-  }
+  }, [errors])
 
   return { setError, removeError, getErrorMessageByFieldName, errors };
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,6 @@
 /* eslint-disable max-len */
 import { Link } from 'react-router-dom';
 
-import { useEffect, useState, useMemo, useCallback } from 'react';
 import {
   Container,
   InputSearchContainer,
@@ -25,84 +24,26 @@ import magnifierQuestion from '../../assets/images/magnifier-question.svg';
 import Loader from '../../components/Loader';
 import Button from '../../components/Button';
 import Modal from '../../components/Modal';
-
-import ContactsService from '../../services/ContactsService';
-
-import toast from '../../utils/toast';
+import useHome from './useHome';
 
 export default function Home() {
-  const [contacts, setContacts] = useState([]);
-  const [orderBy, setOrderBy] = useState('asc');
-  const [searchTerm, setSearchTerm] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
-  const [hasError, setHasError] = useState(false);
-  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
-  const [contactBeingDeleted, setContactBeingDeleted] = useState(null);
-  const [isLoadingDelete, setIsLoadingDelete] = useState(false);
-
-  const filteredContacts = useMemo(
-    () => contacts.filter((contact) => contact.name.toLowerCase().includes(searchTerm.toLowerCase())),
-    [contacts, searchTerm],
-  );
-
-  const loadContacts = useCallback(async () => {
-    try {
-      setIsLoading(true);
-
-      const contactsList = await ContactsService.listContacts(orderBy);
-      await ContactsService.listContacts(orderBy);
-
-      setHasError(false);
-      setContacts(contactsList);
-    } catch {
-      setHasError(true);
-    } finally {
-      setIsLoading(false);
-    }
-  }, [orderBy]);
-
-  useEffect(() => {
-    loadContacts();
-  }, [loadContacts]);
-
-  function handleToggleOrderBy() {
-    const newOrder = orderBy === 'asc' ? 'desc' : 'asc';
-    setOrderBy(newOrder);
-  }
-
-  function handleChangeSearchTerm(event) {
-    setSearchTerm(event.target.value);
-  }
-
-  async function handleTryAgain() {
-    loadContacts();
-  }
-  function handleDeleteContact(contact) {
-    setIsDeleteModalVisible(true);
-    setContactBeingDeleted(contact);
-  }
-
-  function handleCloseDeleteModal() {
-    setIsDeleteModalVisible(false);
-    setContactBeingDeleted(null);
-  }
-
-  async function handleConfirmDeleteContact() {
-    try {
-      setIsLoadingDelete(true);
-      await ContactsService.deleteContact(contactBeingDeleted.id);
-
-      setContacts((prevState) => prevState.filter((contact) => contact.id !== contactBeingDeleted.id));
-
-      handleCloseDeleteModal();
-
-      toast({ type: 'success', text: 'Contato deletado com sucesso!' });
-    } catch {
-      toast({ type: 'danger', text: 'Ocorreu um erro ao deletar o contato!' });
-    } finally {
-      setIsLoadingDelete(false);
-    }
-  }
+  const {
+    isLoadingDelete,
+    isDeleteModalVisible,
+    contactBeingDeleted,
+    handleCloseDeleteModal,
+    handleConfirmDeleteContact,
+    isLoading,
+    searchTerm,
+    handleChangeSearchTerm,
+    hasError,
+    contacts,
+    handleTryAgain,
+    filteredContacts,
+    orderBy,
+    handleToggleOrderBy,
+    handleDeleteContact,
+  } = useHome();
 
   return (
     <Container>
